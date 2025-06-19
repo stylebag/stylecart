ee8e69582ff712f1698937ec30dcaff340206e50
stylebag <tastycam99@gmail.com>
2025-06-19 11:42:49 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,9 +24,7 @@ function renderCard(product) {
                 </div>
             </div>
             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                // <div class="text-center">
-                //     <a class="btn btn-outline-dark mt-auto" href="" target="_blank">${product["btn"]}</a>
-                // </div>
+              
             </div>
         </div>
     </div>`;
